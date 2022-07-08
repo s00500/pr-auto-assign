1078ef830c4f69bbe3dd6572fe02266bf5fbb167
Cameron Larsen <cameron@larsenfam.org>
2022-07-08 10:52:30 -0600

    update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-echo "Building src/${PWD##*/} for linux/amd64..."
-env GOOS=linux GOARCH=amd64 go build -o bin/${PWD##*/}-linux-amd64 .
-echo "Building src/${PWD##*/} for linux/arm64..."
-env GOOS=linux GOARCH=arm64 go build -o bin/${PWD##*/}-linux-arm64 .
-echo "Building src/${PWD##*/} for darwin/amd64..."
-env GOOS=darwin GOARCH=arm64 go build -o bin/${PWD##*/}-darwin-amd64 .
-echo "Building src/${PWD##*/} for darwin/arm64..."
-env GOOS=darwin GOARCH=arm64 go build -o bin/${PWD##*/}-darwin-arm64 .
+cd src
+echo "Building src/pr-auto-assign for linux/amd64..."
+env GOOS=linux GOARCH=amd64 go build -o ../bin/pr-auto-assign-linux-amd64 .
+echo "Building src/pr-auto-assign for linux/arm64..."
+env GOOS=linux GOARCH=arm64 go build -o ../bin/pr-auto-assign-linux-arm64 .
+echo "Building src/pr-auto-assign for darwin/amd64..."
+env GOOS=darwin GOARCH=arm64 go build -o ../bin/pr-auto-assign-darwin-amd64 .
+echo "Building src/pr-auto-assign for darwin/arm64..."
+env GOOS=darwin GOARCH=arm64 go build -o ../bin/pr-auto-assign-darwin-arm64 .
+cd ..
