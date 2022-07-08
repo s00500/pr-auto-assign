48cd88a58802e141c89e8385ceabd0d833d5fd71
Cameron Larsen <cameron@larsenfam.org>
2022-07-08 11:13:36 -0600

    Add auto-release workflow

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ![codeql](https://github.com/cam3ron2/pr-auto-assign/actions/workflows/codeql-analysis.yml/badge.svg)
 ![tests](https://github.com/cam3ron2/pr-auto-assign/actions/workflows/auto-assign.yml/badge.svg)
+![build](https://github.com/cam3ron2/pr-auto-assign/actions/workflows/build-release.yml/badge.svg)
 
 A Github action that will automatically assign a PR to its creator, and assign review to specified users, teams, or a combination of both.
 
--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-cd src
-echo "Building src/pr-auto-assign for linux/amd64..."
+cd dist
+echo "Building dist/pr-auto-assign for linux/amd64..."
 env GOOS=linux GOARCH=amd64 go build -o ../bin/pr-auto-assign-linux-amd64 .
-echo "Building src/pr-auto-assign for linux/arm64..."
+echo "Building dist/pr-auto-assign for linux/arm64..."
 env GOOS=linux GOARCH=arm64 go build -o ../bin/pr-auto-assign-linux-arm64 .
-echo "Building src/pr-auto-assign for darwin/amd64..."
+echo "Building dist/pr-auto-assign for darwin/amd64..."
 env GOOS=darwin GOARCH=arm64 go build -o ../bin/pr-auto-assign-darwin-amd64 .
-echo "Building src/pr-auto-assign for darwin/arm64..."
+echo "Building dist/pr-auto-assign for darwin/arm64..."
 env GOOS=darwin GOARCH=arm64 go build -o ../bin/pr-auto-assign-darwin-arm64 .
 cd ..
